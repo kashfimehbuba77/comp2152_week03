385b247bd07ecdc6661d9fdd09dd290fe2ab6549
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-01-24 09:25:20 -0500

    Creating weapon items

--- lab03.py
+++ lab03.py
@@ -3,3 +3,6 @@ diceOptions = list(range(1,7))
 
 #Weapons Array
 weapons = ("Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb")
+
+#Display available weapons
+print("Available weapons: ", ', '.join(weapons))
