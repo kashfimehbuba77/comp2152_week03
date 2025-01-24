6e012210857676286fd05e41414f1809d4ee72bf
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-01-24 09:31:27 -0500

    Creating validation for user input

--- lab03.py
+++ lab03.py
@@ -6,3 +6,16 @@ weapons = ("Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb")
 
 #Display available weapons
 print("Available weapons: ", ', '.join(weapons))
+
+def get_combat_strength(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= 6:
+                return value
+            else:
+                print("Invalid input. Please enter a number between 1 and 6.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 6")   
+combatStrength = get_combat_strength("Enter your combat strength (1-6): ")
+mCombatStrength = get_combat_strength("Enter monster's combat strength (1-6): ")    
